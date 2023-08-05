cd2f0778abf9296d9c9ad042865d51f28f2793b3
j0sepc0sta <138076450+j0sepc0sta@users.noreply.github.com>
2023-08-05 04:33:39 +0100

    Add files via upload

---
+++ codigos/codigo_c.cpp
@@ -0,0 +1,12 @@
+#include <algorithm>
+#include <vector>
+
+int main()
+{
+    std::vector<int>nums;
+    num.push_back(2);
+    num.push_back(4);
+    num.push_back(3);
+    num.push_back(1);
+    std::sort(nums.begin(), nums.end())
+}
---
+++ codigos/exer2.m
@@ -0,0 +1,5 @@
+function f=exer2(t,z)
+
+f=[z(2); -9.8+0.2*z(1)]
+
+end
